af2f67a4d6361bf8bb9741e9e41411a00a1ac1e6
Rohith Raju <98241736+punisher007007@users.noreply.github.com>
2024-07-28 17:59:56 -0400

    Update index.ts

--- data/index.ts
+++ data/index.ts
@@ -139,27 +139,27 @@ export const testimonials = [
 ];
 
 export const companies = [
-  {
+  /*{
     id: 1,
     name: "Headtstarter AI",
     img: "/ai.jpg",
     nameImg: "/ai.jpg",
     
-  },
-  {
+  },*/
+  /*{
     id: 2,
     name: "appwrite",
     img: "/sims.jpg",
     nameImg: "/ai.jpg",
     
-  },
-  {
+  },*/
+  /*{
     id: 3,
     name: "HOSTINGER",
     img: "/fau.png",
     nameImg: "/ai.jpg",
     
-  },
+  },*/
   /*{
     id: 4,
     name: "stream",
