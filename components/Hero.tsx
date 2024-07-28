1681c9639cf46f999c17ed74294b58dddf252866
Rohith Raju <98241736+punisher007007@users.noreply.github.com>
2024-07-28 16:40:05 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -43,7 +43,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Rohith
           </p>
 
           {/**
@@ -52,12 +52,14 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Welcome to my portfolio This is Rohith Raju"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            I am a Full Stack Developer with a proven track record of leading
+            the development of 11+ web applications using Java, .NET, MERN,
+            Next.js, and Angular.js
           </p>
 
           <a href="#about">
