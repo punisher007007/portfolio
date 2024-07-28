5855c352e0ce31da5f6e334b1a3236e6afdf0ade
Rohith Raju <98241736+punisher007007@users.noreply.github.com>
2024-07-28 17:17:35 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,8 +17,8 @@ const Footer = () => {
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
-          Looking for a dedicated <span className="text-purple">software engineer?</span>
-          Let's discuss how can i contribute to your team.
+          Ready to take <span className="text-purple">your</span> digital
+          presence to the next level?
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to me today and let&apos;s discuss how I can help you
@@ -34,7 +34,7 @@ const Footer = () => {
       </div>
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Rohith Raju
+          Copyright © 2024 Adrian Hajdin
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
