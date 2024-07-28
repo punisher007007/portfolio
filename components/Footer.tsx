1c980ede5424692956f9044f69e3e925dd6e0551
Rohith kanchanapalli <98241736+punisher007007@users.noreply.github.com>
2024-07-28 18:13:13 -0400

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
         </h1>
         <p className="text-white-200 md:mt-10 my-5 text-center">
           Reach out to me today and let&apos;s discuss how I can help you
-          achieve your goals.
+          achieve your project.
         </p>
         <a href="mailto:contact@jsmastery.pro">
           <MagicButton
