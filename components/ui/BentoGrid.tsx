4f995aca3eb2cccff1d3b08ae5bd04ecc94d6728
Rohith kanchanapalli <98241736+punisher007007@users.noreply.github.com>
2024-10-10 07:40:15 -0400

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -66,7 +66,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "rohithraajk@gmail.com";
+    const text = "rohitrajusb32@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
